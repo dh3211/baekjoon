8cfcc4bde030e49a754f862335c2a539789df7ad
daeho <tksxkaos369@gmail.com>
2025-02-11 06:48:36 +0900

    fix: double blank issue
    make_baekjoon.py
    funchtions.py

--- __baekjoon__/make_problem.py
+++ __baekjoon__/make_problem.py
@@ -3,54 +3,70 @@ from bs4 import BeautifulSoup
 import os
 import shutil
 import requests
+import json
 
 
 def main():
     ##  connect problem URL
     problem_id = 15486
 
-
     url = f"https://www.acmicpc.net/problem/{problem_id}"
     headers = {'User-Agent':
                'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
                 AppleWebKit/537.36 (KHTML, like Gecko)\
                 Chrome/132.0.0.0\
-                Safari/537.36'}
+                Safari/537.36',
+               'Referer': 'https://www.acmicpc.net'}
 
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        problem_title = soup.find('span', {'id': 'problem_title'}).decode_contents()
-        soup = img_tag2url(soup, url)
-        soup = remove_button(soup)
-        html_content = str(soup)
-        html_content = cut_content(html_content)
-        html_content = remove_blank(soup, html_content)
-
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    cookies_path = os.path.join(current_dir, 'cookies.json')
+    if os.path.exists(cookies_path):
+        with open(cookies_path, 'r') as file:
+            loaded_cookies = json.load(file)
+            response = requests.get(url, headers=headers, cookies=loaded_cookies)
     else:
+        print(f"{cookies_path} no exist.")
+        response = requests.get(url, headers=headers)
+
+
+    if response.status_code != 200:
         print(f"Failed to request the problem page. Status code: {response.status_code}")
         return
 
+    #  save received_cookies
+    received_cookies = response.cookies.get_dict()
+    with open(cookies_path, "w") as file:
+        json.dump(received_cookies, file, indent=4)
+    print("Cookies saved to cookies.json")
+
+    # process html
+    soup = BeautifulSoup(response.text, 'html.parser')
+    problem_title = soup.find('span', {'id': 'problem_title'}).get_text()
+    soup = img_tag2url(soup, url)
+    soup = remove_button(soup)
+    soup = remove_blank(soup)
+    html_content = str(soup)
+    html_content = cut_content(html_content)
 
     ##  connect problem difficulty image URL
     url = f"https://solved.ac/search?query={problem_id}"
     response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        soup_image = BeautifulSoup(response.text, 'html.parser')
-        image_url = extract_img_url(soup_image, url)
-
-        html_content = \
-            f'''<h1>''' +\
-            f'''<img src="{image_url[9:-1]}" style="height: 1em; vertical-align: middle;" />''' +\
-            f'''  {problem_id}번 - {problem_title}</h1>''' +\
-            f'''\n<br><br>\n''' + html_content
-    
-    else:
+    if response.status_code != 200:
         print(f"Failed to request the difficulty image page. Status code: {response.status_code}")
         return
+    
+    soup_image = BeautifulSoup(response.text, 'html.parser')
+    image_url = extract_img_url(soup_image, url)
+    html_content = \
+        f'''<h1>''' +\
+        f'''<img src="{image_url}" style="height: 1em; vertical-align: middle;" />''' +\
+        f'''  {problem_id}번 - {problem_title}''' +\
+        f'''</h1>''' +\
+        f'''\n<br><br>\n'''+\
+        html_content
 
+    # print(html_content)
 
     ##  create problem folder
     folder_name = '0'*(5- len(str(problem_id))) +f'{problem_id}' +f' - {problem_title}'
@@ -59,9 +75,9 @@ def main():
 
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-        print(f"'{folder_path}' 폴더가 생성되었습니다.")
+        print(f"make {folder_name} directory")
     else:
-        print(f"'{folder_path}' 폴더가 이미 존재합니다.")
+        print(f"{folder_name} exists")
 
 
     ##   create md file
@@ -78,6 +94,8 @@ def main():
     if not os.path.exists(py_path):
         shutil.copy(form_python, py_path)
         print(f"create python file!")
+    else:
+        print(f"python file exists")
 
 
     ##  create IO folder
@@ -90,16 +108,16 @@ def main():
     num = html_content.count('sample-input')
     for i in range(1, num+1):
         pre_tag = soup.find('pre', class_='sampledata', id=f'sample-input-{i}')
-        content = pre_tag.decode_contents()
+        content = pre_tag.get_text()
         with open(os.path.join(IOfolder_path, f'{problem_id}_{i}_i.txt'), 'w', encoding='utf-8') as file:
             file.write(content)
 
         pre_tag = soup.find('pre', class_='sampledata', id=f'sample-output-{i}')
-        content = pre_tag.decode_contents()
+        content = pre_tag.get_text()
         with open(os.path.join(IOfolder_path, f'{problem_id}_{i}_o.txt'), 'w', encoding='utf-8') as file:
             file.write(content)
 
-    print(f'{folder_name}' +" ...Done!")
+    print(f" ...Done!")
 
 
 if __name__ == "__main__":
